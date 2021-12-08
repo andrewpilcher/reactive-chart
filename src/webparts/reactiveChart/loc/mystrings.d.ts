097853936c8bfca0e8476fc485512357a1141491
Andrew Pilcher <Andrew.Pilcher@csulb.edu>
2021-12-08 09:20:59 -0800

    init

---
+++ src/webparts/reactiveChart/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IReactiveChartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ReactiveChartWebPartStrings' {
+  const strings: IReactiveChartWebPartStrings;
+  export = strings;
+}
