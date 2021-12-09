457c020bb523da3ca017d0f0ed6cc5708195eba0
Andrew Pilcher <Andrew.Pilcher@csulb.edu>
2021-12-08 18:10:35 -0800

    get some list items

---
+++ src/webparts/reactiveChart/components/Chart.tsx
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import styles from './Chart.module.scss';
+import { escape } from '@microsoft/sp-lodash-subset';
+import SharePointService from '../../../services/SharePoint/SharePointService';
+import { IListItem } from '../../../services/SharePoint/IListItem';
+
+export interface IChartProps {
+    chartTitle: string;
+}
+
+export interface IChartState {
+    items: IListItem[];
+    loading: boolean;
+    error: string | null;
+}
+
+export default class Chart extends React.Component<IChartProps, IChartState> {
+    constructor(props: IChartProps) {
+        super(props);
+        // bind methods
+        this.getItems = this.getItems.bind(this);
+
+        // set initial state
+        this.state = {
+            items: [],
+            loading: false,
+            error: null,
+        };
+    }
+    public render(): React.ReactElement<IChartProps> {
+        return (
+            <div className={styles.chartTitle}>
+                <h1>{escape(this.props.chartTitle)}</h1>
+                {this.state.error && <p>{this.state.error} </p>}
+                <ul>
+                    {this.state.items.map(item => {
+                        return (
+                            <li key={item.Id}>
+                                <strong>{item.Title}</strong> ({item.Id})
+                            </li>
+
+                        );
+                    })}
+                </ul>
+                <button onClick={this.getItems} disabled={this.state.loading}>{this.state.loading ? 'Loading...' : 'Refresh'}</button>
+            </div>
+        );
+    }
+
+    public getItems(): void {
+        this.setState({ loading: true });
+        SharePointService.getListItems('97140218-63A4-4732-BF07-720E33FA95B3').then(
+            items => {
+                this.setState({ error: null, items: items.value, loading: false });
+            }
+        ).catch(error =>  {
+            this.setState({error: error, loading: false});
+        });
+    }
+}
--- src/webparts/reactiveChart/components/ReactiveChart.tsx
+++ src/webparts/reactiveChart/components/ReactiveChart.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import styles from './ReactiveChart.module.scss';
 import { IReactiveChartProps } from './IReactiveChartProps';
 import { escape } from '@microsoft/sp-lodash-subset';
+import Chart from './Chart';
 
 export default class ReactiveChart extends React.Component<IReactiveChartProps, {}> {
   public render(): React.ReactElement<IReactiveChartProps> {
@@ -10,16 +11,18 @@ export default class ReactiveChart extends React.Component<IReactiveChartProps, 
         <div className={ styles.container }>
           <div className={ styles.row }>
             <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to SharePoint!</span>
-              <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
+              <Chart chartTitle="Welcome to Charts"/>
               <p className={ styles.description }>{escape(this.props.description)}</p>
               <a href="https://aka.ms/spfx" className={ styles.button }>
-                <span className={ styles.label }>Learn more</span>
+                <span className={ styles.label }>Refresh</span>
               </a>
             </div>
           </div>
         </div>
+        
       </div>
     );
   }
+
+  
 }
