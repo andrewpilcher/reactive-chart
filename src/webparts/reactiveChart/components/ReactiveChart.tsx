571a4a082e1c6029609b780e75d84fdc52ada24c
Andrew Pilcher <Andrew.Pilcher@csulb.edu>
2021-12-13 14:19:22 -0800

    remove spfx scaffold styles

--- src/webparts/reactiveChart/components/ReactiveChart.tsx
+++ src/webparts/reactiveChart/components/ReactiveChart.tsx
@@ -7,26 +7,13 @@ import Chart from './Chart';
 export default class ReactiveChart extends React.Component<IReactiveChartProps, {}> {
   public render(): React.ReactElement<IReactiveChartProps> {
     return (
-      <div className={styles.reactiveChart}>
-        <div className={styles.container}>
-          <div className={styles.row}>
-            <div className={styles.column}>
-              <Chart
-                description={this.props.description}
-                listId={this.props.listId}
-                selectedFields={this.props.selectedFields}
-                chartType={this.props.chartType}
-                chartTitle={this.props.chartTitle}
-                chartColors={this.props.chartColors} />
-              <p className={styles.description}>{escape(this.props.description)}</p>
-              <a href="https://aka.ms/spfx" className={styles.button}>
-                <span className={styles.label}>Refresh</span>
-              </a>
-            </div>
-          </div>
-        </div>
-
-      </div>
+      <Chart
+      description={this.props.description}
+      listId={this.props.listId}
+      selectedFields={this.props.selectedFields}
+      chartType={this.props.chartType}
+      chartTitle={this.props.chartTitle}
+      chartColors={this.props.chartColors} />
     );
   }
 
